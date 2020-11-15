fa537c09ed4b887e924c5c9f8d7e9ac50b288ddb
petarPAN <petarburic554@gmail.com>
2020-11-15 20:08:54 +0100

    Add function for interaction between beetle and ball

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const gameBoard = document.querySelector('#gameBoard');
 const playAgain = document.querySelector('#playAgain');
 const btns = document.querySelectorAll('.btns');
 const end = document.querySelector('#end');
+
+function isTouching(a, b) {
+    const aRect = a.getBoundingClientRect();
+    const bRect = b.getBoundingClientRect();
+
+    return !(
+        aRect.top + aRect.height < bRect.top ||
+        aRect.top > bRect.top + bRect.height ||
+        aRect.left + aRect.width < bRect.left ||
+        aRect.left > bRect.left + bRect.width
+    );
+}
