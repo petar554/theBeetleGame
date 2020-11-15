80cd94a0ddf3028fb719b9fb8c3a47af7eb4d79a
petarPAN <petarburic554@gmail.com>
2020-11-15 20:01:37 +0100

    Select elements from html file

---
+++ script.js
@@ -0,0 +1,6 @@
+const head = document.querySelector('#heading');
+const beetle = document.querySelector('#beetle');
+const gameBoard = document.querySelector('#gameBoard');
+const playAgain = document.querySelector('#playAgain');
+const btns = document.querySelectorAll('.btns');
+const end = document.querySelector('#end');
